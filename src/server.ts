90c1c1c6ca9fa9ba5de67e59406daa8193c16f78
aashraful349 <aashraful349@gmail.com>
2026-08-01 16:16:23 +0600

    prisma setup complete

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+
+
+
+const port=config.port;
+
+
+
+async function main(){
+    try {
+        await prisma.$connect();
+        app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`);
+    })
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+
+
+main();
